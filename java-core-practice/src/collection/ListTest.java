416c9a6693d022d2e705d3f0f81b6c6a81915f9f
wankang <1926184697@qq.com>
2017-09-02 18:09:43 +0800

    测试idea与GitHub的整合

---
+++ java-core-practice/src/collection/ListTest.java
@@ -0,0 +1,11 @@
+package collection;
+
+/**
+ * Created by wan_kang on 2017/9/2.
+ */
+public class ListTest {
+    public static void main(String[] args){
+        System.out.println("hello idea");
+    }
+
+}
